5444ce1ab36030b225de05540c12d222f6bc59b4
mauricio-zamora <141055068+mzamora-cnftc@users.noreply.github.com>
2024-10-23 02:02:16 +0000

    Ejericio de Tour

---
+++ Fundamentos/src/semana07/Tour.java
@@ -0,0 +1,13 @@
+package semana07;
+
+import generico.Utils;
+
+public class Tour {
+    public static void main(String[] args) {
+        int numero;
+
+        numero = Utils.leerRangosEnteros("Digite la cantidad de papas: ",1, 2);
+
+        System.out.println(numero);
+    }
+}
